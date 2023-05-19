3b0f64847da648dd6b2324c31e5aef92f550bde7
Rasmus L <rasmuslond1@gmail.com>
2023-05-19 16:51:43 +0300

    first commit

---
+++ Week15/Program.cs
@@ -0,0 +1,58 @@
+﻿List<Movie> myMovies = new List<Movie>();
+string[] data = GetDataFromMyFile();
+//ReadDataFromArray(data);
+
+foreach (string line in data)
+{
+    string[] tempArray = line.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+    Movie newMovie = new Movie(tempArray[0], tempArray[1]);
+    myMovies.Add(newMovie);
+}
+
+foreach (Movie movie in myMovies)
+{
+    Console.WriteLine($"One of my favourite movies '{movie.Title}' has rating {movie.Rating}");
+}
+
+static string[] GetDataFromMyFile()
+{
+    string filePath = @"C:\\Data\movies.txt";
+    return File.ReadAllLines(filePath);
+}
+
+static void ReadDataFromArray(string[] someArray)
+{
+    foreach (string line in someArray)
+    {
+        Console.WriteLine(line);
+    }
+}
+
+class Movie
+{
+    string title; //fields
+    string rating;
+    string year;
+
+    public string Title
+    {
+        get { return title; }
+    }
+
+    public string Year
+    {
+        get { return year; }
+    }
+
+    public string Rating
+    {
+        get { return rating; }
+    }
+
+
+    public Movie(string _title, string _rating)
+    {
+        title = _title;
+        rating = _rating;
+    }
+}
